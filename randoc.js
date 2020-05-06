c1724b21a35ff6259b5e5e11b8aa04096e104887
Rabea Gleissner <rabeagleissner@gmail.com>
2020-05-06 14:56:52 +0800

    Abstract generation of Chance.js values

--- randoc.js
+++ randoc.js
@@ -1,19 +1,18 @@
 const chance = new require('chance')();
 
-const defaultForUnknownField = ({ fieldName, type, array = false }) => {
-  console.log(`No Chance.js generator found for field ${fieldName} of type ${type}. Using string instead.`);
-  return { [fieldName]: array ? [chance.string()] : chance.string() };
+const getChance = ({ fieldName, type, args, array }) => {
+  if (!chance[type]) {
+    console.log(`No Chance.js generator found for field ${fieldName} of type ${type}. Using string instead`);
+    return array ? chance.string() : { [fieldName]: chance.string() };
+  }
+  return array ? chance[type](args) : { [fieldName]: chance[type](args) };
 }
 
 const arrayField = ({ _type, _array, args }, fieldName) => {
   if (_array.empty && !chance.bool({ likelihood: _array.empty }) ) {
     return { [fieldName]: [] }
   }
-  if (chance[_type]) {
-    return { [fieldName]: [...new Array(_array.length || 1)].map(_ => chance[_type](args)) };
-  } else {
-    return defaultForUnknownField({ fieldName, type: _type, array: true });
-  }
+  return { [fieldName]: [...new Array(_array.length || 1)].map(_ => getChance({ fieldName, type: _type, args, array: true })) };
 }
 
 const specialRandomDocument = (fieldName, schema) => {
@@ -26,10 +25,7 @@ const specialRandomDocument = (fieldName, schema) => {
   if (schema._type === 'enum') {
     return { [fieldName]: chance.pickone(schema.options) };
   }
-  if (chance[schema._type] && schema.args) {
-    return { [fieldName]: chance[schema._type](schema.args) };
-  }
-  return defaultForUnknownField({ fieldName, type: schema._type });
+  return getChance({ fieldName, type: schema._type, args: schema.args });
 };
 
 const randomDocument = (fieldSchema) => Object.entries(fieldSchema).reduce((record, [fieldName, type]) => {
@@ -41,10 +37,7 @@ const randomDocument = (fieldSchema) => Object.entries(fieldSchema).reduce((reco
       ? Object.assign({}, record, specialRandomDocument(fieldName, type))
       : Object.assign({}, record, { [fieldName]: randomDocument(type) });
   }
-  if (chance[type]) {
-    return Object.assign({}, record, { [fieldName]: chance[type]() });
-  }
-  return Object.assign({}, record, defaultForUnknownField({ fieldName, type }));
+  return Object.assign({}, record, getChance({ fieldName, type }));
 }, {});
 
 const randomDocuments = (schema, n) => [...Array(n)].map(() => randomDocument(schema));
